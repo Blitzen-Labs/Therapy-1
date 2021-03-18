ca07433f53f49f8560154d1154cd090e5459056a
Lucas <voggavcg601@gmail.com>
2021-03-18 09:49:19 -0300

    Cors fix

--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,12 @@ import { UserController } from './controller/userController';
 const router = Router();
 const userController = new UserController();
 
-router.post('/user', userController.create);
-router.delete('/delete/:id', userController.delete);
-router.post('/update', userController.update);
+router.post('/user', userController.create); //Criar
+router.delete('/delete/:id', userController.delete); //Deletar
+router.post('/update', userController.update); //Atualizar
 
-router.get('/search', userController.search);
-router.get('/show', userController.show);
+router.get('/search', userController.search); // Pesquisar (logar)
+router.get('/show', userController.show); //exibir todos
 
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import 'reflect-metadata';
-
 import './database';
 import { router } from './routes';
 
 const app = express();
+const cors = require('cors')
 
 app.use(express.json());
 app.use(router);
+app.use(cors());
 
 app.listen(12334, () => {
     console.log("Server is running!")
